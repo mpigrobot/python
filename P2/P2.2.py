8cbc1b1cd06e6be58700e64824755ed25ed829bf
hello-piger <gmj@mpig.com.cn>
2017-09-05 08:29:11 +0800

    P2
    The code of lecture three

---
+++ P2/P2.2.py
@@ -0,0 +1,7 @@
+d = {
+    'Adam': 95,
+    'Lisa': 85,
+    'Bart': 59
+}
+d['Paul']=72
+print d
---
+++ P2/P2.4.py
@@ -0,0 +1,11 @@
+months = set(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+x1 = 'Feb'
+x2 = 'Sun'
+if x1 in months:
+    print 'x1: ok'
+else:
+    print 'x1: error'
+if x2 in months:
+    print 'x2: ok'
+else:
+    print 'x2: error'
